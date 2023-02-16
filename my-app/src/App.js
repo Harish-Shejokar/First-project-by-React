399487a074b66ad957b442be3cb104dd3b525952
Harish-Shejokar <hshejokar@gmail.com>
2023-02-16 16:53:25 +0530

    first-commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import ExpenseItem from './Components/ExpenseItem';
+import ListItem from './Components/ListItem';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get Started</h2>
+      <ExpenseItem></ExpenseItem>
+      <ListItem></ListItem>
+    </div>
+  )
+  }
+export default App;
---
+++ my-app/src/Components/ListItem.js
@@ -0,0 +1,11 @@
+function ListItem() {
+  return (
+    <ul>
+      <li>Food Rs 10</li>
+      <li>Petrol Rs 100</li>
+      <li>Movies Rs 200</li>
+    </ul>
+  );
+}
+
+export default ListItem;
