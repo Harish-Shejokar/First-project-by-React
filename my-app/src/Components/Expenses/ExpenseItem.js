17d36aef39628a737a229f755f0662eb22edd79a
Harish-Shejokar <hshejokar@gmail.com>
2023-02-20 15:12:12 +0530

    understate useState in depthand also placed button which changes expense to

--- my-app/src/Components/Expenses/ExpenseItem.js
+++ my-app/src/Components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 
@@ -6,24 +7,34 @@ import './ExpenseItem.css'
 
 
 const ExpenseItem = (props) =>{
-
+  const [title , setTitle] = useState(props.title)
+  console.log("code reEvaluation ");
+  
   const clickHandler = ()=>{
-    console.log("working")
+    setTitle("New-title");
+    console.log(title);
   };
 
-  const DeleteExpense = ()=>{
-    // console.log(props.id)
-    const child = document.getElementById(props.id);
-    child.remove();
+  // const DeleteExpense = ()=>{
+  //   // console.log(props.id)
+  //   const child = document.getElementById(props.id);
+  //   child.remove();
+  // }
+  
+  const [amount, setAmount] = useState(props.amount)
+
+  const changeAmount = ()=>{
+      setAmount('$100');
   }
   
   return (
     <Card  className="expense-item" id={props.id}>
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title={props.title} amount={props.amount} location={props.location}
+       <ExpenseDetails title={title} amount={amount} location={props.location}
       id={props.id} />
+      <button onClick = {changeAmount} >$100</button>
       <button onClick = {clickHandler} >ChangeTitle</button>
-      <button onClick = {DeleteExpense} >DeleteExpense</button>
+      {/* <button onClick = {DeleteExpense} >DeleteExpense</button> */}
     </Card>
   );
 }
