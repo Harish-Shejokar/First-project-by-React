fe6ccaa86e3ac577200d775b0bb6b6b8bb299140
Harish-Shejokar <hshejokar@gmail.com>
2023-02-22 15:58:08 +0530

    task 10 completed understand need of key in map when display data on UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,54 +7,52 @@ import NewExpense from "./Components/NewExpense/NewExpense";
 // import logo from './logo.svg';
 import "./App.css";
 
-const App = () => {
-  const expense = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      LocationOfExpenditure: "Delhi",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      LocationOfExpenditure: "Pune",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      LocationOfExpenditure: "Hydrabad",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      LocationOfExpenditure: "Mumbai",
-    },
-  ];
-  // var Data = [{}];
-  // const [expense,addExpense] = useState([{
-  //     id: "e1",
-  //     title: "Toilet Paper",
-  //     amount: 94.12,
-  //     date: new Date(2020, 7, 14)
-  //   }]);
+const Dummy_Expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    LocationOfExpenditure: "Delhi",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    LocationOfExpenditure: "Pune",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    LocationOfExpenditure: "Hydrabad",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    LocationOfExpenditure: "Mumbai",
+  },
+];
 
-  const addExpenseHandler = data =>{
+const App = () => {
+ 
+ const [expense, setExpenses] = useState(Dummy_Expenses);
+
+  const addExpenseHandler = (enteredExpense) =>{
     // console.log("In App.js");
-    console.log(data);
-    // expense=[data];
-    // Data = [{...data}];
-    // console.log(Data);
+    // console.log(data);
+    console.log(enteredExpense.date);
+    // enteredExpense.date = Date.parse(enteredExpense.date);
+
+    setExpenses((prevExpense)=>{
+      return [enteredExpense,
+        ...prevExpense]
+    });
     
-    
-    // addExpense(Data);
   }
 
   return (
--- my-app/src/Components/Expenses/Expense.js
+++ my-app/src/Components/Expenses/Expense.js
@@ -18,25 +18,21 @@ const Expense = (props) => {
 
   return (
     <Card className="expense">
-      <ExpensesFilter selected={filteredYear} selectedYear ={year} />   
+      <ExpensesFilter selected={filteredYear} selectedYear ={year} />
+         
       {props.items.map((expense) => {
+      
         return (
           <ExpenseItem
             // location={expense.LocationOfExpenditure}
-            id = {expense.id}
+            key={expense.id}
+            id = {Math.random().toString()}
             date={expense.date}
             title={expense.title}
             amount={expense.amount}
-          ></ExpenseItem>
+          /> 
         );
       })}
-
-      {/* <ExpenseItem location={expense[1].LocationOfExpenditure} date={expense[1].date} title={expense[1].title} amount={expense[1].amount}></ExpenseItem>
-    
-          <ExpenseItem location={expense[2].LocationOfExpenditure} date={expense[2].date} title={expense[2].title} amount={expense[2].amount}></ExpenseItem>
-    
-          <ExpenseItem location={expense[3].LocationOfExpenditure} date={expense[3].date} title={expense[3].title} amount={expense[3].amount}></ExpenseItem>
-         */}
     </Card>
   );
 }
