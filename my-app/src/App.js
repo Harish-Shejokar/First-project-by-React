5c2a7e58303ec032ba67049f8a7168ba3ef4ce23
Harish-Shejokar <hshejokar@gmail.com>
2023-02-23 08:52:58 +0530

    added filter method or just filtering array of Objects

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,7 +12,7 @@ const Dummy_Expenses = [
     id: "e1",
     title: "Toilet Paper",
     amount: 94.12,
-    date: new Date(2020, 7, 14),
+    date: new Date(2019, 7, 14),
     LocationOfExpenditure: "Delhi",
   },
   {
@@ -26,7 +26,7 @@ const Dummy_Expenses = [
     id: "e3",
     title: "Car Insurance",
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2022, 2, 28),
     LocationOfExpenditure: "Hydrabad",
   },
   {
@@ -54,7 +54,7 @@ const App = () => {
     });
     
   }
-  console.log(expense);
+  // console.log(expense);
 
   return (
     <div>
--- my-app/src/Components/Expenses/Expense.js
+++ my-app/src/Components/Expenses/Expense.js
@@ -1,40 +1,52 @@
 // import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
-import './Expense.css';
+import "./Expense.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import React ,{ useState } from "react";
+import React, { useState } from "react";
 
 const Expense = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
 
-  const [filteredYear , setFilteredYear] =useState('2020');
+  const year = (year) => {
+    setFilteredYear(year);
+    // console.log(year);
+  };
+ 
   
+ 
   
-  const year = (year)=>{
-     setFilteredYear(year)
-     console.log(year);
+  const Arr = props.items.filter(fun);
+  
+  function fun(elem){
+    console.log(filteredYear);
+    console.log(elem.date.getFullYear())
+    if(elem.date.getFullYear().toString() === filteredYear.toString()) return elem;
+    // return elem
   }
+  // console.log(Arr);
+   
 
+ 
 
   return (
     <Card className="expense">
-      <ExpensesFilter selected={filteredYear} selectedYear ={year} />
-         
-      {props.items.map((expense) => {
-      
+      <ExpensesFilter selected={filteredYear} selectedYear={year} />
+
+      {Arr.map((expense) => {
         return (
           <ExpenseItem
             // location={expense.LocationOfExpenditure}
             key={expense.id}
-            id = {Math.random().toString()}
+            id={Math.random().toString()}
             date={expense.date}
             title={expense.title}
             amount={expense.amount}
-          /> 
+          />
         );
       })}
     </Card>
   );
-}
+};
 
 export default Expense;
